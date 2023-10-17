8f4f01aa0887bc9daaa051ddaf5d073f49aa64ca
rrmorant <duecel@icloud.com>
2023-10-17 15:38:03 -0700

    second codes

--- higher-order-jas-ron.js
+++ higher-order-jas-ron.js
@@ -13,14 +13,43 @@ const arr1 = [3, 9, 15, 4, 10]
 // console.log(arr2)
 // output: [1.5, 4.5, 7.5, 2, 5] COMPLETE
 
-const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
+// const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
 
-const oddArr = (arr2) => {
-    return arr2.map(value => {
-        if (value % 2 !== 0) {
-            return value
-        }
-    })
+// const oddArr = (arr2) => {
+//     return arr2.map(value => {
+//         if (value % 2 !== 0) {
+//             return value
+//         }
+//     })
+// }
+// console.log(oddArr(arr2))
+// // output: [7, 3, 5, 13, -9]
+
+// const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
+// // output: [7, 3, 5, 13, -9]
+
+// const oddNums = (array) => {
+//   return array.filter((value) => value % 2 !== 0)
+
+// }
+// console.log(oddNums(arr2)) //[ 7, 3, 5, 13, -9 ]
+
+const pumbaa = "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze"
+// output: ["a", "wonderful", "ain't", "passing", "craze"]
+
+// const oddNumWord = pumbaa.split(" ")
+
+// 
+const oddNum = (array) => {
+    return array.filter((value) => value % 2 !== 0)   
 }
-console.log(oddArr(arr2))
-// output: [7, 3, 5, 13, -9]
+console.log(oddNum(pumbaa.split(" ".length)))
+
+// [
+//     'Hakuna',    'Matata',
+//     'what',      'a',
+//     'wonderful', 'phrase',
+//     'Hakuna',    'Matata',
+//     "ain't",     'no',
+//     'passing',   'craze'
+//   ]
