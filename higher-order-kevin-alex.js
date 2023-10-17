f7fc56ce518a9d69a353f74b8a3deeb095984edb
kevinrojas <k3vinr07@gmail.com>
2023-10-17 13:31:31 -0700

    completed first 7 problems

---
+++ higher-order-kevin-alex.js
@@ -0,0 +1,76 @@
+// const arr1 = [3, 9, 15, 4, 10]
+// // output: [30, 90, 150, 40, 100]
+
+// const multTen = (array) =>{
+//     return array.map((value => value *10))
+// }
+// console.log(multTen(arr1))
+
+//2
+// const arr1 = [3, 9, 15, 4, 10]
+// // output: [1.5, 4.5, 7.5, 2, 5]
+
+// const divTwo = (array) => {
+//     return array.map((value) => value /2)
+// }
+// console.log(divTwo(arr1))
+
+//3
+// const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
+// // output: [7, 3, 5, 13, -9]
+
+// const onlyOdd = (array) => {
+//     return array.filter((value) => value %2 !== 0)
+// }
+// console.log(onlyOdd(arr2))
+
+
+//4
+// const pumbaa =
+//   "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze"
+// // output: ["a", "wonderful", "ain't", "passing", "craze"]
+
+// const oddChar = (array) => {
+//     return (array.split(" ")).filter((value) => value.length%2 !== 0)
+// }
+// console.log(oddChar(pumbaa))
+
+
+//5
+// const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// // output: "nicework"
+
+// const onlyString = (array) =>{
+//     return (array.filter((value) => typeof value === "string")).join("")
+// }
+// console.log(onlyString(comboArr))
+
+
+//6
+// const filterArrayValues = [58, "", "abcd", true, null, false, 0]
+// // output: [58, "abcd", true]
+// const someThing = (array) => {
+//     return array.filter((value) => value)
+// }
+// console.log(someThing(filterArrayValues))
+
+
+// 7
+// const makesWackyWords = ["simba", "nala", "zazu", "rafiki"]
+// // output: ["sImBa", "nAlA", "zAzU", "rAfIkI"]
+
+// const everyOther = (array) => {
+//     return array.map((value) => value.slice(0,1) + (value.slice(1)).toUpperCase()
+//     )
+// }
+// console.log(everyOther(makesWackyWords))
+
+
+//8
+const str = "javascript is awesome"
+// output: "jvscrpt s wsm"
+
+const noVowels = (str) => {
+    return 
+}
+console.log(noVowels(str))
