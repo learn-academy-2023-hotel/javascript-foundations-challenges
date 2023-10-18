3007d3c640f5eff81ca7b5b5fcdc3b257adfc2f4
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-18 15:40:36 -0700

    objects challenges

---
+++ objects-joseph-danny.js
@@ -0,0 +1,43 @@
+// const person = {
+//     firstName: "Arthur",
+//     lastName: "Dent"
+//   }
+//   Write the code that accesses the first name of the person object.
+
+// console.log(person.firstName)
+
+// Write the code that accesses the last name of the person object.
+
+// console.log(person.lastName)
+
+// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+
+// person.homePlanet = 'Earth'
+
+// console.log(person.homePlanet)
+
+// console.log(person)
+
+// const person = {
+//     firstName: "Arthur",
+//     lastName: "Dent",
+//     arthursHome: function () {
+//         return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+//     }
+//   }
+// person.homePlanet = 'Earth'
+
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
+// console.log(person.arthursHome())
+
+const product = {
+    name: "chair",
+    price: 24.99
+  }
+//   Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+// const describeProduct = (value) => {
+//     return `The product is a ${value.name}. It costs ${value.price}.`
+// }
+// console.log(describeProduct(product))
+
+// Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
