00a8160c668e5a2c24367158728668fb6e928286
rrmorant <duecel@icloud.com>
2023-10-18 16:43:29 -0700

    added challenges

--- objects-dalton-ron.js
+++ objects-dalton-ron.js
@@ -1,10 +1,12 @@
 // //Challenge-1
-// // const person = {
-// //     firstName: "Arthur",
-// //     lastName: "Dent",
-// //     homePlanet: "Earth",
-// //   }
+// const person = {
+//     firstName: "Arthur",
+//     lastName: "Dent",
+//     homePlanet: "Earth",
+//   }
 // // console.log(person.firstName)
+
+// const { object } = require("prop-types")
 
 // // console.log(person.lastName)
 
@@ -21,3 +23,14 @@
 // }
 
 // console.log(describeProduct())
+const lunch = {
+    name: "PB and Banana",
+    type: "sandwich",
+    ingredients: ["bread", "peanut butter", "banana"]
+  }
+//   console.log(lunch.ingredients.join(" ")) //bread peanut butter banana
+//   console.log(lunch.ingredients[2]) //banana
+const lunchRecipe = (lunchObject) => {
+    return `The ingredients for a ${lunch.name} ${lunch.type} are ${lunch.ingredients.join(" ")}`
+}
+console.log(lunchRecipe(lunch)) // The ingredients for a PB and Banana sandwich are bread peanut butter banana
