27638f0e3ac17beb39d3111e8d5a26089bdd4280
DaltonMcauliffe <dalton.j.mcauliffe97@gmail.com>
2023-10-18 15:38:42 -0700

    challenge 1 & 2

---
+++ objects-dalton-ron.js
@@ -0,0 +1,23 @@
+// //Challenge-1
+// // const person = {
+// //     firstName: "Arthur",
+// //     lastName: "Dent",
+// //     homePlanet: "Earth",
+// //   }
+// // console.log(person.firstName)
+
+// // console.log(person.lastName)
+
+// // console.log(person.firstName, person.lastName, "is from planet", person.homePlanet)
+
+// //challenge-2
+
+// const product = {
+//     name: "chair",
+//     price: 24.99
+// }
+//    const describeProduct = () =>   {
+//     return `The product is a ${product.name} it cost ${product.price}`  
+// }
+
+// console.log(describeProduct())
