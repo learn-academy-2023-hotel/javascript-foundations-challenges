d568e03ace34dbb265bc6668d183f9d77715d856
alexnguyen <alexanderdnguyen4@gmail.com>
2023-10-19 17:55:21 -0700

    Made some corrections

--- classes-oscar-alex.js
+++ classes-oscar-alex.js
@@ -1,44 +1,44 @@
 // Coffee Maker: copy the given Coffee class into a text editor
 
-// class Coffee {
-//   constructor(type, cream, sugar) {
-//     this.type = type.toLowerCase()
-//     this.cream = cream
-//     this.sugar = sugar
-//   }
+class Coffee {
+  constructor(type, cream, sugar) {
+    this.type = type.toLowerCase()
+    this.cream = cream
+    this.sugar = sugar
+  }
 
-//   coffeeProfile() {
-//     return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
-//   }
+  coffeeProfile() {
+    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+  }
 
-//   creams() {
-//     if (this.cream > 1) {
-//       return `${this.cream} creams`
-//     } else {
-//       return `${this.cream} cream`
-//     }
-//   }
+  creams() {
+    if (this.cream > 1) {
+      return `${this.cream} creams`
+    } else {
+      return `${this.cream} cream`
+    }
+  }
 
-//   sugars() {
-//     if (this.sugar > 1) {
-//       return `${this.sugar} sugars`
-//     } else {
-//       return `${this.sugar} sugar`
-//     }
-//   }
-// }
+  sugars() {
+    if (this.sugar > 1) {
+      return `${this.sugar} sugars`
+    } else {
+      return `${this.sugar} sugar`
+    }
+  }
+}
 
 // // Write the code that makes a black coffee object
-// let blackCoffee = new Coffee("black", "no", "no")
+let blackCoffee = new Coffee ("black", 0, 0)
 
 // // Write the code that outputs the black coffee's profile
-// console.log(blackCoffee)
+console.log(blackCoffee.coffeeProfile())
 
 // // Write the code that makes a coffee object with 1 cream and 2 sugars
-// let blondeCoffee = new Coffee ("black", "1", "2")
+let blondeCoffee = new Coffee ("Arabica", 1, 2)
 
 // // Write the code that outputs the 1 cream and 2 sugars coffee profile
-// console.log(blondeCoffee)
+console.log(blondeCoffee.coffeeProfile())
 
 
 // // Latte Maker: create a class for Latte
@@ -69,28 +69,28 @@
 
 // Volume of a Cylinder: create a class for Cylinder
 
-class Cylinder {
-    constructor(radiusNumber, heightNumber) {
-        this.radius = radiusNumber
-        this.height = heightNumber
-    }
-    volume () {
-        return Math.PI * this.radius ** 2 * this.height
-    }
-}
+// class Cylinder {
+//     constructor(radiusNumber, heightNumber) {
+//         this.radius = radiusNumber
+//         this.height = heightNumber
+//     }
+//     volume () {
+//         return Math.PI * this.radius ** 2 * this.height
+//     }
+// }
 
-let firstVol = new Cylinder (2, 4)
-// console.log(firstVol.volume().toFixed(4))
+// let firstVol = new Cylinder (2, 4)
+// // console.log(firstVol.volume().toFixed(4))
 
-let cylOne = new Cylinder (1, 5)
-let cylTwo = new Cylinder (3, 6)
-let cylThree = new Cylinder (5, 10)
-console.log(cylOne)
-console.log(cylOne.volume().toFixed(4))
-console.log(cylTwo)
-console.log(cylTwo.volume().toFixed(4))
-console.log(cylThree)
-console.log(cylThree.volume().toFixed(4))
+// let cylOne = new Cylinder (1, 5)
+// let cylTwo = new Cylinder (3, 6)
+// let cylThree = new Cylinder (5, 10)
+// console.log(cylOne)
+// console.log(cylOne.volume().toFixed(4))
+// console.log(cylTwo)
+// console.log(cylTwo.volume().toFixed(4))
+// console.log(cylThree)
+// console.log(cylThree.volume().toFixed(4))
 // Write a class that calculates the volume of a Cylinder: v = πr^2h (r is the radius and h is the height of the cylinder)
 // Write the code that rounds the volume of the cylinder to four decimal places
 // Write the code that creates three unique cylinder objects
